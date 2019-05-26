875c100c3f579a403e651de7b7a70f27684c886b
naim.uddin@lubyc.com <naim.uddin@lubyc.com>
2019-05-26 23:27:03 +0600

    Update database connection file

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.listen(PORT, () => {
     // mongoose.connect('mongodb://localhost:27017/money-management-app', { useNewUrlParser: true }, () => {
     //     console.log('Database Connected...')
     // });
-    mongoose.connect(`mongodb+srv://${process.env.dbUsername}:${dbPassword}@monyappcluster-zn1vp.mongodb.net/test?retryWrites=true`, { useNewUrlParser: true }, () => {
+    mongoose.connect(`mongodb+srv://${process.env.dbUsername}:${process.env.dbPassword}@monyappcluster-zn1vp.mongodb.net/test?retryWrites=true`, { useNewUrlParser: true }, () => {
         console.log('Database Connected...')
     });
 })
